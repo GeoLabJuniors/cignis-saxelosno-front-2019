8ad5ddf93f885d9dd972310154342f3f2c7c9965
iosebi2899 <ge4988@gmail.com>
2019-06-19 19:52:44 +0400

    კატალოგის გვერდი

--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@
 $(".photo").click(function () {
     $(".search").toggleClass("active")
 });
+
+$(document).ready(function(){
+		  $('.gallery').slick({
+			centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  adaptiveHeight: true,
+  autoplay: true,
+  autoplaySpeed: 430,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+			});
+		});
